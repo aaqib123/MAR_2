7b78b214bed817dd86c54ef851c42a240597e3b2
Aaqib <aaqib.hyd@gmail.com>
2016-03-23 16:37:25 -0300

    MAR_2
    ionic/firebase/auth0 app for medication reminders

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\aaqib\Documents\NetBeansProjects\MAR_2\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\aaqib\Documents\NetBeansProjects\MAR_2\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\aaqib\Documents\NetBeansProjects\MAR_2\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\aaqib\Documents\NetBeansProjects\MAR_2\platforms\android\AndroidManifest.xml \
